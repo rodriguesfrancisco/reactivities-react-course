b671c8b9a4fcf8b9df99e976b47a4be216099edf
Francisco Rodrigues <f.co.victor@hotmail.com>
2020-06-22 08:54:31 -0300

    Initial styles

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import { Header, Icon, List } from 'semantic-ui-react'
+import { List, Container } from 'semantic-ui-react'
 import { IActivity } from '../models/activity';
+import NavBar from '../../features/nav/NavBar';
 
 
 const App = () => {
@@ -15,17 +16,16 @@ const App = () => {
     }, []);
 
     return (
-        <div>
-            <Header as='h2'>
-                <Icon name='users' />
-                <Header.Content>Reactivities</Header.Content>
-            </Header>
-            <List>
-                {activities.map((activity: IActivity) => (
-                    <List.Item key={activity.id}>{activity.title}</List.Item>
-                ))}
-            </List>
-        </div>
+        <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+                <List>
+                    {activities.map((activity: IActivity) => (
+                        <List.Item key={activity.id}>{activity.title}</List.Item>
+                    ))}
+                </List>
+            </Container>
+        </Fragment>
     );
 
 }
