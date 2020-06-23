cc2aab927db288295f8372be37e2a1a37f71bc3b
Francisco Rodrigues <f.co.victor@hotmail.com>
2020-06-23 11:26:00 -0300

    Selecting an activity

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -6,19 +6,21 @@ import { ActivityDetails } from '../details/ActivityDetails'
 import { ActivityForm } from '../form/ActivityForm'
 
 interface IProps {
-    activities: IActivity[]
+    activities: IActivity[];
+    selectActivity: (id: string) => void;
+    selectedActivity: IActivity | null;
 }
 
-export const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
+export const ActivityDashboard: React.FC<IProps> = ({ activities, selectActivity, selectedActivity }) => {
     return (
         <Grid>
             <Grid.Column width={10}>
                 <List>
-                    <ActivityList activities={activities} />
+                    <ActivityList activities={activities} selectActivity={selectActivity} />
                 </List>
             </Grid.Column>
             <Grid.Column width={6}>
-                <ActivityDetails />
+                {selectedActivity && <ActivityDetails activity={selectedActivity} />}
                 <ActivityForm />
             </Grid.Column>
         </Grid>
--- src/features/activities/dashboard/ActivityList.tsx
+++ src/features/activities/dashboard/ActivityList.tsx
@@ -3,11 +3,12 @@ import { Item, Button, Label, Segment } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
 
 interface IProps {
-    activities: IActivity[]
+    activities: IActivity[];
+    selectActivity: (id: string) => void;
 }
 
 
-export const ActivityList: React.FC<IProps> = ({ activities }) => {
+export const ActivityList: React.FC<IProps> = ({ activities, selectActivity }) => {
     return (
         <Segment clearing>
             <Item.Group divided>
@@ -21,7 +22,12 @@ export const ActivityList: React.FC<IProps> = ({ activities }) => {
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button floated='right' content='View' color='blue' />
+                                <Button
+                                    onClick={() => selectActivity(activity.id)}
+                                    floated='right'
+                                    content='View'
+                                    color='blue'
+                                />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
